1dbef822cb9367c1cab04ceb1dc25b55dc80010a
Joshua Musa <gidijosh@gmail.com>
2021-09-13 16:24:44 +0100

    feat: add mvoie component

---
+++ src/components/Movie/Movie.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Movie({ movie, openModal }) {
+  return (
+    <div
+      className="card"
+      onClick={() => {
+        openModal(movie);
+      }}
+    >
+      <img
+        className="card--image"
+        src={`${process.env.REACT_APP_API_BASE_IMAGE_URL}/${movie.poster_path}`}
+        alt={movie.title}
+      />
+      <span className="card--rating">{movie.vote_average}</span>
+      <div className="card--container">
+        <p>{movie.title}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
